896cb4fe05afb689cd910245e0812368a7d83922
MiMi-Yup <tuivanthiet@gmail.com>
2024-06-01 16:29:54 +0700

    Revert "(...) Update code: Change Postgresql -> Sqlite"
    This reverts commit f980c1f0823b85614881a01f0917ef8451bcadda.

--- API/Extensions/ServiceExtensions.cs
+++ API/Extensions/ServiceExtensions.cs
@@ -44,7 +44,7 @@ namespace API.Extensions
         {
             services.AddDbContext<ConnectifyDbContext>(options =>
             {
-                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
             });
         }
 
